67248d61f9eee7835813cf968c2d6d7bbfc97375
fimovol <fimovolpe@gmail.com>
2022-12-30 13:39:52 -0500

    camobios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
